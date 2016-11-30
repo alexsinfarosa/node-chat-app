ce9266805c5dcd565b565de4ffc4b8cf120c19ca
alexsinfarosa <as898@cornell.edu>
2016-11-30 15:27:04 -0500

    Add greet new user and alert others

--- public/js/index.js
+++ public/js/index.js
@@ -2,12 +2,6 @@ const socket = io();
 
 socket.on('connect', () => {
   console.log('Connect to the server');
-
-  socket.emit('createMessage', {
-    from: 'Alex',
-    text: 'Facci quella bella'
-  });
-
 });
 
 socket.on('disconnect', () => {
--- server/server.js
+++ server/server.js
@@ -15,13 +15,23 @@ io.on('connection', (socket) => {
   console.log('New user connected');
 
   socket.emit('newMessage', {
-    from: 'alex@gmail.com',
-    text: 'Do not forget',
-    createdAt: 123
+    from: 'Admin',
+    text: 'Welcome to the chat app',
+    createdAt: new Date().getTime()
   });
 
+  socket.broadcast.emit('newMessage', {
+    from: 'Admin',
+    from: 'User joined',
+    createdAt: new Date().getTime()
+  })
+
   socket.on('createMessage', (message) => {
-    console.log(message);
+    io.emit('newMessage', {
+      from: message.from,
+      text: message.text,
+      createdAt: new Date().getTime()
+    });
   });
 
   socket.on('disconnect', () => {
