87f33c1a98afd66face23c8a21e82c2077212dbb
alexsinfarosa <as898@cornell.edu>
2016-12-01 11:36:16 -0500

    Add form and show incoming messages in browser

--- public/js/index.js
+++ public/js/index.js
@@ -1,13 +1,52 @@
 const socket = io();
 
 socket.on('connect', () => {
-  console.log('Connect to the server');
+  console.log('Connected to server');
 });
+
+// socket.emit('createMessage', {
+//   from: 'Alex',
+//   text: 'Hi'
+// }, (data) => {
+//   console.log(data);
+// });
+
+// socket.on('newMessage', (message) => {
+//   console.log(message);
+// });
 
 socket.on('disconnect', () => {
   console.log('Disconnect from server');
 });
 
-socket.on('newMessage', (message) => {
-  console.log(message);
+// VUE
+new Vue ({
+  el: '#app',
+  data: {
+    messages: [],
+    message: ''
+  },
+  methods: {
+
+    createMessage () {
+      if (this.message != "") {
+        socket.emit('createMessage', {
+        from: 'Alex',
+        text: this.message
+        }, (data) => {
+          // console.log('createMessage', data);
+        })
+      }
+      this.message = '';
+    }
+
+  },
+
+  created () {
+    socket.on('newMessage', (message) => {
+      console.log(message);
+      this.messages.push(message);
+    });
+  }
+
 });
