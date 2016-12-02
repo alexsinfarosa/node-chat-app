342818596ed2a34f4f0034b085ddbfc00e122fc4
alexsinfarosa <as898@cornell.edu>
2016-12-02 15:48:34 -0500

    Changed folder structure

--- client/app.js
+++ client/app.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import {sync} from 'vuex-router-sync'
-import App from './views/App'
+import App from './App.vue'
 import router from './router'
 import store from './store'
 sync(store, router)
